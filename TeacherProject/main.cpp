534d087b506a66f3df786057a47496d8be84b6f4
judyscheurm <cody.henrichsen@740s-pl-129.local>
2016-07-26 13:16:31 -0600

    used sandwich class to main method

--- TeacherProject/main.cpp
+++ TeacherProject/main.cpp
@@ -8,24 +8,37 @@
 
 #include <iostream>
 #include <string>
+#include "Sandwich.h"
 
 using namespace std;
 
 int main()
 {
     // insert code here...
-    cout << "Hello, World!" << endl;
-    double myFraction;
-    cout << myFraction << endl;
-    myFraction = 3.15;
-    cout << myFraction << endl;
-    int myFavoriteNumber;
-    cout << myFavoriteNumber << endl;
-    cout << "Please type in your favorite number.";
-    cin >> myFavoriteNumber;
-    cout << myFavoriteNumber <<endl;
-    string myWords;
-    myWords = "Wow! This is so much fun!";
-    cout << myWords << endl;
+//    cout << "Hello, World!" << endl;
+//    double myFraction;
+//    cout << myFraction << endl;
+//    myFraction = 3.15;
+//    cout << myFraction << endl;
+//    int myFavoriteNumber;
+//    cout << myFavoriteNumber << endl;
+//    cout << "Please type in your favorite number.";
+//    cin >> myFavoriteNumber;
+//    cout << myFavoriteNumber <<endl;
+//    string myWords;
+//    myWords = "Wow! This is so much fun!";
+//    cout << myWords << endl;
+    
+    Sandwich mySandwich;
+    cout  <<mySandwich.getName() << endl;
+    cout << "Let's rename my sandwich" <<endl;
+    cout << "Please type a new name" <<endl;
+    string tempName;
+    // cin >> tempName;
+    getline (cin, tempName);
+    mySandwich.setName(tempName);
+    cout << "The new name is: " << mySandwich.getName() << endl;
+    
+    
     return 0;
 }
